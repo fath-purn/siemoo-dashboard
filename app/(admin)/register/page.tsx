13ade29da4127b128250e371c335e6fb04c2784f
Fatkhurrohaman Purnomo <talitakamilia345@gmail.com>
2024-02-09 18:36:40 +0700

    kasaran

--- app/(admin)/register/page.tsx
+++ app/(admin)/register/page.tsx
@@ -1,4 +1,3 @@
-import { LogoFull } from '@/app/ui/svg-image';
 import LoginForm from '@/app/ui/admin/login-form';
 import { Metadata } from 'next';
  
@@ -12,7 +11,6 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-            <LogoFull />
           </div>
         </div>
         <LoginForm />
